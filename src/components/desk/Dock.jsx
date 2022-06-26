e3df6d397294a9c2aeb71f37fbcabf5fcff7b865
E. Berke KARAGÖZ <34271483+BerkeKaragoz@users.noreply.github.com>
2022-06-26 15:50:47 +0300

    fix: grow dock

--- src/components/desk/Dock.jsx
+++ src/components/desk/Dock.jsx
@@ -3,7 +3,7 @@ import { Card, CardBody, CardHeader } from "reactstrap";
 
 export default function Dock({ header, className, children, ...rest }) {
   return (
-    <Card className={"rounded-0 " + (className ?? "")} {...rest}>
+    <Card className={"rounded-0 flex-grow-1" + (className ?? "")} {...rest}>
       {header && <CardHeader className="p-1">{header}</CardHeader>}
       <CardBody className="p-1">{children}</CardBody>
     </Card>
